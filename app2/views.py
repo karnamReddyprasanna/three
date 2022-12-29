90106a4c8fc9a5a9e487d84f5be7319b1bd32efb
karnamReddyprasanna <prasanareddy43@gmail.com>
2022-12-29 21:09:52 +0530

    first commit

---
+++ app2/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def third(request):
+    return render(request,'h2.html')
+def third(request):
+    return render(request,'h3.html')
